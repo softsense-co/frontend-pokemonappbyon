fd3e78a4fdb1f3c1c32d79a8b34768ff0cf4a164
muhammad <muhammadbyon31@gmail.com>
2023-05-15 21:54:46 +0700

    first commit

--- src/Components/Content/Poke.js
+++ src/Components/Content/Poke.js
@@ -1,116 +1,146 @@
-import React from "react";
-import Swal from 'sweetalert2';
-
+import Swal from "sweetalert2";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Poke() {
-  const pokemons = [
-    {
-      id: "1",
-      name: "Pokemon Api",
-      image: require("../../img/11.png"),
-      price: "$30",
-    },
-    {
-      id: "2",
-      name: "Pokemon Air",
-      image: require("../../img/13.png"),
-      price: "$20",
-    },
-    {
-      id: "3",
-      name: "Pokemon Angin",
-      image: require("../../img/16.png"),
-      price: "$25",
-    },
-    {
-      id: "4",
-      name: "Pokemon Udara",
-      image: require("../../img/12.png"),
-      price: "$39",
-    },
-    {
-      id: "5",
-      name: "Pokemon Batu",
-      image: require("../../img/15.png"),
-      price: "$36",
-    },
-    {
-      id: "6",
-      name: "Pokemon Tangan",
-      image: require("../../img/14.png"),
-      price: "$40",
-    },
-    {
-      id: "7",
-      name: "Pokemon Wowo",
-      image: require("../../img/17.webp"),
-      price: "$45",
-      isNew: true,
-    },
-    {
-      id: "8",
-      name: "Pokemon Wewe",
-      image: require("../../img/18.webp"),
-      price: "$50",
-      isNew: true,
-    },
-    {
-      id: "9",
-      name: "Pokemon Wiwi",
-      image: require("../../img/9.png"),
-      price: "$45",
-      isNew: true,
-    },
-    {
-      id: "10",
-      name: "Pokemon Wawa",
-      image: require("../../img/10.png"),
-      price: "$50",
-      isNew: true,
-    },
-  ];
+  // const pokemons = [
+
+  //   {
+  //     id: "1",
+  //     name: "Pokemon Api",
+  //     image: require("../../img/11.png"),
+  //     price: "$30",
+  //   },
+  //   {
+  //     id: "2",
+  //     name: "Pokemon Air",
+  //     image: require("../../img/13.png"),
+  //     price: "$20",
+  //   },
+  //   {
+  //     id: "3",
+  //     name: "Pokemon Angin",
+  //     image: require("../../img/16.png"),
+  //     price: "$25",
+  //   },
+  //   {
+  //     id: "4",
+  //     name: "Pokemon Udara",
+  //     image: require("../../img/12.png"),
+  //     price: "$39",
+  //   },
+  //   {
+  //     id: "5",
+  //     name: "Pokemon Batu",
+  //     image: require("../../img/15.png"),
+  //     price: "$36",
+  //   },
+  //   {
+  //     id: "6",
+  //     name: "Pokemon Tangan",
+  //     image: require("../../img/14.png"),
+  //     price: "$40",
+  //   },
+  //   {
+  //     id: "7",
+  //     name: "Pokemon Wowo",
+  //     image: require("../../img/17.webp"),
+  //     price: "$45",
+  //     isNew: true,
+  //   },
+  //   {
+  //     id: "8",
+  //     name: "Pokemon Wewe",
+  //     image: require("../../img/18.webp"),
+  //     price: "$50",
+  //     isNew: true,
+  //   },
+  //   {
+  //     id: "9",
+  //     name: "Pokemon Wiwi",
+  //     image: require("../../img/9.png"),
+  //     price: "$45",
+  //     isNew: true,
+  //   },
+  //   {
+  //     id: "10",
+  //     name: "Pokemon Wawa",
+  //     image: require("../../img/10.png"),
+  //     price: "$50",
+  //     isNew: true,
+  //   },
+  // ];
+
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    async function fetchPokemons() {
+      try {
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+        const pokemonList = response.data.results;
+        const pokemonData = [];
+
+        for (const pokemon of pokemonList) {
+          const pokemonResponse = await axios.get(pokemon.url);
+          const pokemonImage = pokemonResponse.data.sprites.front_default;
+
+          pokemonData.push({
+            name: pokemon.name,
+            image: pokemonImage,
+          });
+        }
+
+        setPokemons(pokemonData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchPokemons();
+  }, []);
 
   function addPoke(pokemon) {
     Swal.fire({
-      title: 'Confirmation',
-      text: 'Sure you want to add this Pokemon?',
-      icon: 'question',
+      title: "Confirmation",
+      text: "Sure you want to add this Pokemon?",
+      icon: "question",
       showCancelButton: true,
-      confirmButtonText: 'Yes',
-      cancelButtonText: 'No',
-      reverseButtons: true
+      confirmButtonText: "Yes",
+      cancelButtonText: "No",
+      reverseButtons: true,
     }).then((result) => {
       if (result.value) {
         // Ambil data dari local storage (jika ada)
         console.log("pokemon", pokemon);
-        let myPokemonList = JSON.parse(localStorage.getItem("myPokemonList")) || [];
-        const isDataExist = myPokemonList.find((p) => p.id === pokemon.id);
-  
+        let myPokemonList =
+          JSON.parse(localStorage.getItem("myPokemonList")) || [];
+        const isDataExist = myPokemonList.find((p) => p.name === pokemon.name);
+
         if (isDataExist) {
           Swal.fire({
-            title: 'Error',
-            text: 'Pokemon Already Exist!',
-            icon: 'error',
-            confirmButtonText: 'OK'
+            title: "Error",
+            text: "Pokemon Already Exist!",
+            icon: "error",
+            confirmButtonText: "OK",
           });
         } else {
           // Tambahkan pokemon yang dipilih ke dalam array myPokemon
           myPokemonList.push(pokemon);
           localStorage.setItem("myPokemonList", JSON.stringify(myPokemonList));
-  
+
           Swal.fire({
-            title: 'Success',
-            text: 'Successfully Added My Pokemon!',
-            icon: 'success',
-            confirmButtonText: 'OK'
+            title: "Success",
+            text: "Successfully Added My Pokemon!",
+            icon: "success",
+            confirmButtonText: "OK",
           });
         }
       } else if (result.dismiss === Swal.DismissReason.cancel) {
         Swal.fire({
-          title: 'Cancelled',
-          text: 'Adding Pokemon Cancelled!',
-          icon: 'info',
-          confirmButtonText: 'OK'
+          title: "Cancelled",
+          text: "Adding Pokemon Cancelled!",
+          icon: "info",
+          confirmButtonText: "OK",
         });
       }
     });
@@ -141,10 +171,10 @@ function Poke() {
                 <h2 className="text-xl font-semibold mb-2 text-black">
                   {pokemon.name}
                 </h2>
-                <div className="flex justify-between items-center mb-4">
+                {/* <div className="flex justify-between items-center mb-4">
                   <p className="text-gray-600">Price:</p>
                   <p className="text-gray-800">{pokemon.price}</p>
-                </div>
+                </div> */}
                 <div className="flex justify-center">
                   <button
                     onClick={() => addPoke(pokemon)}
